9e389ad2c9d215cdab6089b00279a3a231d72c19
mukut-d <mukut.m.das2000@gmail.com>
2024-03-19 16:05:21 +0530

    now validating JWT token

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,2 +1,39 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+require("dotenv").config();
+
+const serviceAccountKey = require("./serviceAccountKey.json");
+
+const express = require("express");
+const app = express();
+
+// Body parser for our JSON data!
+app.use(express.json());
+
+// cross origin
+const cors = require("cors");
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+// firebase credentials
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+// api endpoints
+app.get("/", (req, res) => {
+  return res.send("hello world");
+});
+
+exports.app = functions.https.onRequest(app);
