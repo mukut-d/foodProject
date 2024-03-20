b1d975f3729a3cb66bc79d7223f328e20c00a7dc
mukut-d <mukut.m.das2000@gmail.com>
2024-03-21 00:49:19 +0530

    finished the header section

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -1,7 +1,9 @@
+import { Header } from "../components";
+
 export default function Main() {
   return (
-    <div className="w-screen min-h-screen flex items-center justify-center flex-col bg-primary">
-      Main
-    </div>
+    <main className="w-screen min-h-screen flex items-center justify-center flex-col bg-primary">
+      <Header />
+    </main>
   );
 }
