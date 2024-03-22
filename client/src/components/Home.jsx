478dcebc60225a2dfd2b52b3f8e071d43084b970
mukut-d <mukut.m.das2000@gmail.com>
2024-03-23 01:06:39 +0530

    started working on home slider

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
-import { buttonClick } from "../animations";
-import { Delivery } from "../assets";
+import { buttonClick, staggerFadeInOut } from "../animations";
+import { Delivery, HeroBg } from "../assets";
+import { randomData } from "../util/styles";
 
 const Home = () => {
   return (
@@ -44,7 +45,42 @@ const Home = () => {
           </motion.button>
         </div>
 
-        <div>2</div>
+        <div className="py-2 flex-1 flex items-center justify-end relative">
+          <img
+            className="absolute top-0 right-0 md:right-12 w-full h-420 md:w-auto md:h-650"
+            src={HeroBg}
+            alt=""
+          />
+          <div className="w-full md:w-460 ml-0 flex flex-wrap items-center justify-center gap-4 gap-y-14">
+            {randomData &&
+              randomData.map((data, i) => {
+                return (
+                  <motion.div
+                    key={i}
+                    {...staggerFadeInOut(i)}
+                    className="w-32 h-36 md:h-auto md:w-190 p-4 bg-lightOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+                  >
+                    <img
+                      src={data.imageURL}
+                      className="w-12 h-12 md:h-12 md:-mt-16 object-contain"
+                      alt=""
+                    />
+                    <p className="text-sm lg:text-xl font-semibold text-textColor">
+                      {data.product_name.slice(0, 14)}
+                    </p>
+                    <p className="text-[12px] text-center  md:text-base text-lighttextGray font-semibold  capitalize">
+                      {data.product_category}
+                    </p>
+
+                    <p className="text-sm  font-semibold text-headingColor">
+                      <span className="text-xs text-red-600">$</span>{" "}
+                      {data.product_price}
+                    </p>
+                  </motion.div>
+                );
+              })}
+          </div>
+        </div>
       </motion.div>
     </>
   );
