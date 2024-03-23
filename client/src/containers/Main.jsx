a843b7c0c75ebc00f2122b9c85297c5fcedcb7b4
mukut-d <mukut.m.das2000@gmail.com>
2024-03-23 16:26:00 +0530

    completed the home slider

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -1,11 +1,27 @@
-import { Header, Home } from "../components";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../api";
+import { Header, Home, HomeSlider } from "../components";
+import { setAllProducts } from "../context/actions/productActions";
 
 export default function Main() {
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!products) {
+      getAllProducts().then((data) => {
+        dispatch(setAllProducts(data));
+      });
+    }
+  }, []);
+
   return (
     <main className="w-screen min-h-screen flex items-center justify-start flex-col bg-primary">
       <Header />
       <div className="w-full flex flex-col items-start justify-center mt-40 px-6 md:px-24 2xl:px-96 gap-12 pb-24 ">
         <Home />
+        <HomeSlider />
       </div>
     </main>
   );
